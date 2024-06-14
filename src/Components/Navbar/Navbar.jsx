2dc4833d56cf92c9ed85147c43542706510b3436
mactrix-markjohn <chukwuemekaokparaeke@gmail.com>
2024-06-14 20:40:28 +0100

    Added the Nav active selection

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,20 +7,25 @@ import { useState } from 'react'
 const Navbar = () => {
 
   const [mobileMenu, setMobileMenu] = useState(false)
+  
   const toggleMenu = ()=>{
     mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
   }
+
+  const [menu,setMenu] = useState("home");
+
+
 
 
   return (
     <nav className='container whitenav'>
         <Link to={'/laisla'}><img src={logo} alt='' className='logo' /></Link>
         <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
-            <Link to={'/laisla'}><li>HOME</li></Link>
-            <Link to={'/laisla/PrivateLabel'}><li>PRIVATE LABEL SWIMWEAR</li></Link>
-            <Link to={'/laisla/AboutUs'}><li>ABOUT US</li></Link>
-            <Link to={'/laisla/Journal'}><li>JOURNAL</li></Link>
-            <Link to={'/laisla/ContactUs'}><li>CONTACT US</li></Link>
+            <Link to={'/laisla'}><li className={menu==="home" ? 'active' : ''} onClick={()=>setMenu("home")} >HOME</li></Link>
+            <Link to={'/laisla/PrivateLabel'}><li className={menu==="privatelabel" ? 'active' : ''} onClick={()=>setMenu("privatelabel")}>PRIVATE LABEL SWIMWEAR</li></Link>
+            <Link to={'/laisla/AboutUs'}><li className={menu==="aboutus" ? 'active' : ''} onClick={()=>setMenu("aboutus")}>ABOUT US</li></Link>
+            <Link to={'/laisla/Journal'}><li className={menu==="journal" ? 'active' : ''} onClick={()=>setMenu("journal")}>JOURNAL</li></Link>
+            <Link to={'/laisla/ContactUs'}><li className={menu==="contactus" ? 'active' : ''} onClick={()=>setMenu("contactus")}>CONTACT US</li></Link>
         </ul>
         <img src={navmenu} alt='' className='menu-icon' onClick={toggleMenu}/>
     </nav>
