d41ad8a7e427ed4b8d625ec95d22eab7803e8405
mactrix-markjohn <chukwuemekaokparaeke@gmail.com>
2024-07-04 22:54:47 +0100

    Finished Sport Illustrated

--- src/Components/PressPage/PressPageData.js
+++ src/Components/PressPage/PressPageData.js
@@ -1,6 +1,6 @@
-import press1 from '../../assets/press1.jpg'
-import press2 from '../../assets/press2.jpg'
-import press3 from '../../assets/press3.jpg'
+// import press1 from '../../assets/press1.jpg'
+// import press2 from '../../assets/press2.jpg'
+// import press3 from '../../assets/press3.jpg'
 //import press4 from '../../assets/press4.jpg'
 //import press5 from '../../assets/press5.jpg'
 
@@ -16,12 +16,35 @@ import SPIMarisa1 from '../../assets/Sports Illustrated/Sportls Illustrated - Ma
 import SPIMarisa2 from '../../assets/Sports Illustrated/Sportls Illustrated - Marisa Miller/1202862859_press_detail_image3_img059.jpg'
 
 
-
-
 import SPIGigi from '../../assets/Sports Illustrated/Sports Illustrated - Gigi Hadid/SI Cover.png'
+import SPIGigi1 from '../../assets/Sports Illustrated/Sports Illustrated - Gigi Hadid/SI Spread 1.png'
+import SPIGigi2 from '../../assets/Sports Illustrated/Sports Illustrated - Gigi Hadid/SI Spread 2.png'
+import SPIGigi3 from '../../assets/Sports Illustrated/Sports Illustrated - Gigi Hadid/SI Spread 3.png'
+import SPIGigi4 from '../../assets/Sports Illustrated/Sports Illustrated - Gigi Hadid/SI SPREAD.jpg'
+
+
 import SPILucia from '../../assets/Sports Illustrated/Sports Illustrated - Lucia Dvorska/Cover.jpg'
+import SPILucia1 from '../../assets/Sports Illustrated/Sports Illustrated - Lucia Dvorska/09_lucia-dvorska_07.jpg'
+import SPILucia2 from '../../assets/Sports Illustrated/Sports Illustrated - Lucia Dvorska/feature.jpg'
+
+
 import SPIWeb from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_01.jpg'
+import SPIWeb1 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_02.jpg'
+import SPIWeb2 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_03.jpg'
+import SPIWeb3 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_04.jpg'
+import SPIWeb4 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_05.jpg'
+import SPIWeb5 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_06.jpg'
+import SPIWeb6 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_07.jpg'
+import SPIWeb7 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_08.jpg'
+import SPIWeb8 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_09.jpg'
+import SPIWeb9 from '../../assets/Sports Illustrated/Sports Illustrated Web/08_marisa-miller_body_10.jpg'
+
 import SPIBehind from '../../assets/Sports Illustrated/Sports Illustrated Behind the Scenes/Picture 1 13-01-09.png'
+import SPIBehind1 from '../../assets/Sports Illustrated/Sports Illustrated Behind the Scenes/08_marisa-miller_body_behind_01.jpg'
+import SPIBehind2 from '../../assets/Sports Illustrated/Sports Illustrated Behind the Scenes/08_marisa-miller_body_behind_02.jpg'
+import SPIBehind3 from '../../assets/Sports Illustrated/Sports Illustrated Behind the Scenes/08_marisa-miller_body_behind_03.jpg'
+import SPIBehind4 from '../../assets/Sports Illustrated/Sports Illustrated Behind the Scenes/08_marisa-miller_body_behind_04.jpg'
+import SPIBehind5 from '../../assets/Sports Illustrated/Sports Illustrated Behind the Scenes/08_marisa-miller_body_behind_05.jpg'
 
 
 const Press_Page = [
@@ -44,7 +67,7 @@ const Press_Page = [
       {
           j_no:"sports-illustrated-gigi-hadid",
           j_img:SPIGigi,
-          j_gallery: [SPIGigi,press1, press2, press3],
+          j_gallery: [SPIGigi,SPIGigi1, SPIGigi2, SPIGigi3,SPIGigi4],
           j_title:"Sports Illustrated - Gigi Hadid",
           j_body:"",
           j_date:""
@@ -52,7 +75,7 @@ const Press_Page = [
       {
           j_no:"sports-illustrated-lucia-dvorska",
           j_img:SPILucia,
-          j_gallery: [SPILucia,press1, press2, press3],
+          j_gallery: [SPILucia,SPILucia1, SPILucia2],
           j_title:"Sports Illustrated - Lucia Dvorska",
           j_body:"",
           j_date:""
@@ -60,7 +83,7 @@ const Press_Page = [
       {
           j_no:"sports-illustrated-web",
           j_img:SPIWeb,
-          j_gallery: [SPIWeb,press1, press2, press3],
+          j_gallery: [SPIWeb,SPIWeb1, SPIWeb2, SPIWeb3,SPIWeb4,SPIWeb5,SPIWeb6,SPIWeb7,SPIWeb8,SPIWeb9],
           j_title:"Sports Illustrated Web",
           j_body:"",
           j_date:""
@@ -68,7 +91,7 @@ const Press_Page = [
       {
           j_no:"sports-illustrated-behind-the-scenes",
           j_img:SPIBehind,
-          j_gallery: [SPIBehind,press1, press2, press3],
+          j_gallery: [SPIBehind,SPIBehind1, SPIBehind2, SPIBehind3,SPIBehind4,SPIBehind5],
           j_title:"Sports Illustrated Behind the Scenes",
           j_body:"",
           j_date:""
